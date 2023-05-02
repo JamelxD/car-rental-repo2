4ce23859d20fd37aaabbe9acc3b7edabb4370d26
abiodunt97 <104908253+abiodunt97@users.noreply.github.com>
2023-05-02 11:16:52 +0100

    Update bookingConditions.js

--- rentalApp/src/components/CarBooking/bookingConditions.js
+++ rentalApp/src/components/CarBooking/bookingConditions.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export const bookingConditions = () => {
     return (
         <div>
-            <p>You can view the full terms and conditions at the vender's site <Link to={"https://www.sixt.com/ride/bookingconditions/"}> here</Link></p>
+            <p>You can view the full terms and conditions at the vendor's site <Link to={"https://www.sixt.com/ride/bookingconditions/"}> here</Link></p>
         </div>
     )
 }
